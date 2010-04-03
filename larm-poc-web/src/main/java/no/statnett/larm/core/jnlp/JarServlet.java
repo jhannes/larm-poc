8122e759bc53bdefc7f7d3438f51748732047684
Johannes Brodwall <johannes@brodwall.com>
2010-04-03 03:59:45 +0200

    Java Web Start support: * Added servlets to serve JNLP file and JAR-file * Added Maven tasks to package and sign client

---
+++ larm-poc-web/src/main/java/no/statnett/larm/core/jnlp/JarServlet.java
@@ -0,0 +1,64 @@
+package no.statnett.larm.core.jnlp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JarServlet extends HttpServlet {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String jarFile = req.getRequestURI();
+        if (!jarFile.startsWith(req.getContextPath())) {
+            log.error("Huh? Expected " + jarFile + " to start with " + req.getContextPath());
+        } else {
+            jarFile = jarFile.substring(req.getContextPath().length()+1);
+        }
+
+        if (!getAllowedJars().contains(jarFile)) {
+            log.warn("Tried to serve illegal jar file " + jarFile);
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        InputStream result = null;
+
+        File file = new File("../larm-poc-client/target/" + jarFile);
+        if (file.exists()) {
+            log.debug("Found " + file);
+            result = new FileInputStream(file);
+        }
+        if (result == null) {
+            result = getServletContext().getResourceAsStream("/WEB-INF/lib/" + jarFile);
+        }
+        if (result == null) {
+            LoggerFactory.getLogger(getClass()).warn("Tried to serve unknown jar file " + jarFile);
+        } else {
+            copyAndClose(result, resp.getOutputStream());
+        }
+    }
+
+    private void copyAndClose(InputStream inputStream, OutputStream outputStream) throws IOException {
+        byte[] buffer = new byte[1024 * 4];
+        int n;
+        while (-1 != (n = inputStream.read(buffer))) {
+            outputStream.write(buffer, 0, n);
+        }
+        outputStream.flush();
+        inputStream.close();
+    }
+
+    private List<String> getAllowedJars() {
+        ArrayList<String> allowedJars = new ArrayList<String>();
+        allowedJars.add("larm-poc-client-1.0-SNAPSHOT-jar-with-dependencies.jar");
+        return allowedJars;
+    }
+}
---
+++ larm-poc-web/src/main/java/no/statnett/larm/core/jnlp/JnlpServlet.java
@@ -0,0 +1,62 @@
+package no.statnett.larm.core.jnlp;
+
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JnlpServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 6254020517387286254L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+            IOException {
+        String codebaseUrl = "http://" + req.getServerName() + ":" + req.getServerPort() + req.getContextPath();
+        String repositoryUrl = codebaseUrl + "/repository";
+        String servletPath = req.getServletPath();
+        if (!servletPath.endsWith(".jnlp") || !servletPath.startsWith("/")) {
+            throw new RuntimeException("Huh!? " + servletPath);
+        }
+        String className = servletPath.substring(1, servletPath.lastIndexOf(".jnlp"));
+
+        resp.setContentType("application/x-java-jnlp-file");
+
+        Element rootElement = DocumentHelper.createElement("jnlp")
+                .addAttribute("spec", "1.0+")
+                .addAttribute("codebase", codebaseUrl)
+                .addAttribute("href", className + ".jnlp");
+        Element info = rootElement.addElement("information");
+        info.addElement("title").addText("LARM");
+        info.addElement("vendor").addText("Statnett");
+
+        Element resources = rootElement.addElement("resources");
+        resources.addElement("j2se").addAttribute("version", "1.6+");
+        for (String clientJarFile : getClientJarFiles()) {
+            resources.addElement("jar").addAttribute("href", clientJarFile);
+        }
+
+        Element application = rootElement.addElement("application-desc");
+        application.addAttribute("main-class", "no.statnett.larm.poc.client." + className);
+        application.addElement("argument").addText(repositoryUrl);
+
+        rootElement.addElement("security").addElement("all-permissions");
+
+        DocumentHelper.createDocument(rootElement).write(resp.getWriter());
+    }
+
+    private List<String> getClientJarFiles() {
+        ArrayList<String> clientJarFiles = new ArrayList<String>();
+        clientJarFiles.add("larm-poc-client-1.0-SNAPSHOT-jar-with-dependencies.jar");
+        return clientJarFiles;
+    }
+
+
+}
