ab31c768eed997629f70e50347ca135bf7b3f25a
Johannes Brodwall <johannes@brodwall.com>
2010-05-11 21:27:51 +0200

    Generate pom for Hessian

--- dependencies/install-dependencies.bat
+++ dependencies/install-dependencies.bat
@@ -1,3 +1,3 @@
-cmd.exe /c "mvn install:install-file -DgroupId=com.caucho -DartifactId=hessian -Dversion=4.0.3 -Dpackaging=jar -Dfile=%~dp0\hessian-4.0.3.jar"
 cmd.exe /c "mvn install:install-file -DgroupId=com.caucho -DartifactId=hessian -Dversion=4.0.3 -Dpackaging=jar -Dclassifier=sources -Dfile=%~dp0\hessian-4.0.3-src.jar"
+cmd.exe /c "mvn install:install-file -DgroupId=com.caucho -DartifactId=hessian -Dversion=4.0.3 -Dpackaging=jar -Dfile=%~dp0\hessian-4.0.3.jar -DgeneratePom=true"
 
